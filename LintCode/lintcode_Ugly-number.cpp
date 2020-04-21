3032641b110ff2ccb3fcc978dcca45a941cd1a95
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-04-21 14:12:56 +0530

    Update lintcode_Ugly-number.cpp

--- LintCode/lintcode_Ugly-number.cpp
+++ LintCode/lintcode_Ugly-number.cpp
@@ -1,25 +1,47 @@
-/*
-*
-* Tag: Math
-* Time: O(lgn)
-* Space: O(1)
-*/
 class Solution {
 public:
-    /**
-     * @param num an integer
-     * @return true if num is an ugly number or false
+    /*
+     * @param k: The number k.
+     * @return: The kth prime number as description.
      */
-    bool isUgly(int num) {
-        // Write your code here
-        if(!num)
-            return false;
-        for(int i = 0; i < 3; ++ i){
-            while(num%fact[i] == 0)
-                num /= fact[i];
+    long long kthPrimeNumber(int k) {
+        // write your code here
+        if(k == 1)
+            return 3;
+        if(k == 2)
+            return 5;
+        if(k == 3)
+            return 7;
+        priority_queue<long long, vector<long long>, greater<long long>> q;
+        q.push(3);
+        q.push(5);
+        q.push(7);
+        unordered_set<long long> st;
+        st.insert(3);
+        st.insert(5);
+        st.insert(7);
+        int cnt = 0;
+        long long ans;
+        while(!q.empty()){
+            long long u = q.top();
+            q.pop();
+            ++ cnt;
+            ans = u;
+            if(cnt == k)
+                break;
+            if(st.find(3*u) == st.end()){
+                q.push(3*u);
+                st.insert(3*u);
+            }
+            if(st.find(5*u) == st.end()){
+                q.push(5*u);
+                st.insert(5*u);
+            }
+            if(st.find(7*u) == st.end()){
+                q.push(7*u);
+                st.insert(7*u);
+            }
         }
-        return num == 1;
+        return ans;
     }
-private:
-int fact[3]={2,3,5};
-};
+}
